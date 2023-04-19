0d7c6cd00ed4e4c37069161f1e6afd741a727cbb
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-04-19 00:07:15 +0000

    feat: docker file working

--- src/use_cases/open_page/open_page.ts
+++ src/use_cases/open_page/open_page.ts
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer"
 
 export async function openPage(url: string = 'https://studeo.unicesumar.edu.br/#!/access/login') {
 
-    const browser = await puppeteer.launch({headless: false, args: ['--disable-gpu']})
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome', args: ['--no-sandbox']})
     const page = await browser.newPage()
     await page.goto(url)
 
